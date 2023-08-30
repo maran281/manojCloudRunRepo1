3e02c6981258825c3bcbfc695d99f880ab6cdfc0
mukesh sain <mukeshk.sain786@gmail.com>
2023-08-30 21:16:27 +0200

    correction in app.py

--- cloudrun_manoj_poc/app.py
+++ cloudrun_manoj_poc/app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify
 
 app=Flask(__name__)
 
-@app.route("/", method=["GET"])
+@app.route("/", methods=["GET"])
 def hello_world():
     return jsonify(message="hello world!!!"),200
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0',port=int(os.environ.get("port",8080))) 
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT",8080))) 
